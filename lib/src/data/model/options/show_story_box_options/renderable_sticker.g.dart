12fb498af0e76979cf976ed952dea9b1c0f50c64
Aleksandr Yurkovskiy <sanekyy@gmail.com>
2021-08-23 15:41:08 +0300

    regenerate built models

--- lib/src/data/model/options/show_story_box_options/renderable_sticker.g.dart
+++ lib/src/data/model/options/show_story_box_options/renderable_sticker.g.dart
@@ -67,7 +67,7 @@ class _$RenderableStickerSerializer
           break;
         case 'url':
           result.url = serializers.deserialize(value,
-              specifiedType: const FullType(String)) as String;
+              specifiedType: const FullType(String)) as String?;
           break;
         case 'transform':
           result.transform.replace(serializers.deserialize(value,
@@ -77,7 +77,7 @@ class _$RenderableStickerSerializer
           result.clickableZones.replace(serializers.deserialize(value,
                   specifiedType: const FullType(
                       BuiltList, const [const FullType(ClickableZone)]))!
-              as BuiltList<Object>);
+              as BuiltList<Object?>);
           break;
         case 'can_delete':
           result.canDelete = serializers.deserialize(value,
@@ -246,4 +246,4 @@ class RenderableStickerBuilder
   }
 }
 
-// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,lines_longer_than_80_chars,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new
+// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,deprecated_member_use_from_same_package,lines_longer_than_80_chars,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new
